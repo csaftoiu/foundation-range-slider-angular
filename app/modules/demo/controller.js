5123d083cfc2562838cce1e49319a88872129f83
Claudiu <csaftoiu@gmail.com>
2015-11-17 13:11:15 -0500

    First commit

---
+++ app/modules/demo/controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foundation-range-slider-angular.demo.controller:DemoCtrl
+ * @description
+ * # DemoCtrl
+ * Controller of the slider demo.
+ */
+angular.module('foundation-range-slider-angular.demo')
+  .controller('DemoCtrl', function () {
+  })
+;
---
+++ app/modules/demo/demo.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('foundation-range-slider-angular.demo', [
+  'foundation-range-slider-angular'
+])
+
+;
+
