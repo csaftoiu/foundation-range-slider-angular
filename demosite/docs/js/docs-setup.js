96627d4ac2bd4eae39bddda23c1bfbd2a201f7cb
Claudiu <csaftoiu@gmail.com>
2015-11-17 14:19:48 -0500

    push demo site

---
+++ demosite/docs/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "foundation-range-slider-angular",
+      "shortName": "foundation-range-slider-angular",
+      "type": "overview",
+      "moduleName": "foundation-range-slider-angular",
+      "shortDescription": "foundation-range-slider-angular",
+      "keywords": "angular api base foundation foundation-range-slider-angular module overview range-slider"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
