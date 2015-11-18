8774e29c3d961bb73db17997637487c3a1522495
Claudiu <csaftoiu@gmail.com>
2015-11-18 12:50:49 -0500

    Update demosite

--- demosite/docs/js/docs-setup.js
+++ demosite/docs/js/docs-setup.js
@@ -11,6 +11,15 @@ NG_DOCS={
       "moduleName": "foundation-range-slider-angular",
       "shortDescription": "foundation-range-slider-angular",
       "keywords": "angular api base foundation foundation-range-slider-angular module overview range-slider"
+    },
+    {
+      "section": "api",
+      "id": "foundation-range-slider-angular.directive:range-slider",
+      "shortName": "range-slider",
+      "type": "directive",
+      "moduleName": "foundation-range-slider-angular",
+      "shortDescription": "Creates a Foundation range slider",
+      "keywords": "api creates directive foundation foundation-range-slider-angular html http range slider zurb"
     }
   ],
   "apis": {
